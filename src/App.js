c7c56297fe7a8d09f7f9ffce48af3c649a79d55b
juantrochez <juan.trochez95@gmail.com>
2018-02-28 11:19:59 +0100

    remove logs

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ for (let i = 0; i <= GRID_SIZE; i++) {
 const generateNewFinderGrid = () => {
   pfGrid = null;
   pfGrid = new Grid(GRID_SIZE, GRID_SIZE);
-
+  
   for (let i = 0; i < GRID_SIZE-1; i++) {
     for (let j = 0; j < GRID_SIZE-1; j++) {
       if (i == 0 || j == 0 || i == GRID_SIZE-1 || j == GRID_SIZE-1) pfGrid.setWalkableAt(i, j, false);
@@ -65,252 +65,244 @@ const KEY_CODES_MAPPER_OTHER_COMMAND = {
 }
 
 const getRandomNumberFromRange = (min, max) =>
-  Math.floor(Math.random() * (max - min +1 ) + min);
+Math.floor(Math.random() * (max - min +1 ) + min);
 
 const getRandomCoordinate = () =>
-  ({
-    x: getRandomNumberFromRange(1, GRID_SIZE - 1),
-    y: getRandomNumberFromRange(1, GRID_SIZE - 1),
-  });
+({
+  x: getRandomNumberFromRange(1, GRID_SIZE - 1),
+  y: getRandomNumberFromRange(1, GRID_SIZE - 1),
+});
 
 const isBorder = (x, y) =>
-  x === 0 || y === 0 || x === GRID_SIZE || y === GRID_SIZE;
+x === 0 || y === 0 || x === GRID_SIZE || y === GRID_SIZE;
 
 const isPosition = (x, y, diffX, diffY) =>
-  x === diffX && y === diffY;
+x === diffX && y === diffY;
 
 const isSnake = (x, y, snakeCoordinates) =>
-  snakeCoordinates.filter(coordinate => isPosition(coordinate.x, coordinate.y, x, y)).length;
+snakeCoordinates.filter(coordinate => isPosition(coordinate.x, coordinate.y, x, y)).length;
 
 const getSnakeHead = (snake) =>
-  snake.coordinates[0];
+snake.coordinates[0];
 
 const getSnakeWithoutStub = (snake) =>
-  snake.coordinates.slice(0, snake.coordinates.length - 1);
+snake.coordinates.slice(0, snake.coordinates.length - 1);
 
 const getSnakeTail = (snake) =>
-  snake.coordinates.slice(1);
+snake.coordinates.slice(1);
 
 const getIsSnakeOutside = (snake) =>
-  getSnakeHead(snake).x >= GRID_SIZE ||
-  getSnakeHead(snake).y >= GRID_SIZE ||
-  getSnakeHead(snake).x <= 0 ||
-  getSnakeHead(snake).y <= 0;
+getSnakeHead(snake).x >= GRID_SIZE ||
+getSnakeHead(snake).y >= GRID_SIZE ||
+getSnakeHead(snake).x <= 0 ||
+getSnakeHead(snake).y <= 0;
 
 const getIsSnakeClumy = (snake) =>
-  isSnake(getSnakeHead(snake).x, getSnakeHead(snake).y, getSnakeTail(snake));
+isSnake(getSnakeHead(snake).x, getSnakeHead(snake).y, getSnakeTail(snake));
 
 const getIsSnakeEating = ({ snake, snack }) =>
- isPosition(getSnakeHead(snake).x, getSnakeHead(snake).y, snack.coordinate.x, snack.coordinate.y);
+isPosition(getSnakeHead(snake).x, getSnakeHead(snake).y, snack.coordinate.x, snack.coordinate.y);
 
 const getCellCs = (isGameOver, snake, snack, x, y) =>
-  cs(
-    'grid-cell',
-    {
-      'grid-cell-border': isBorder(x, y),
-      'grid-cell-snake': isSnake(x, y, snake.coordinates),
-      'grid-cell-snack': isPosition(x, y, snack.coordinate.x, snack.coordinate.y),
-      'grid-cell-hit': isGameOver && isPosition(x, y, getSnakeHead(snake).x, getSnakeHead(snake).y),
-    }
-  );
+cs(
+  'grid-cell',
+  {
+    'grid-cell-border': isBorder(x, y),
+    'grid-cell-snake': isSnake(x, y, snake.coordinates),
+    'grid-cell-snack': isPosition(x, y, snack.coordinate.x, snack.coordinate.y),
+    'grid-cell-hit': isGameOver && isPosition(x, y, getSnakeHead(snake).x, getSnakeHead(snake).y),
+  }
+);
 
 const setNewPosition = (snakeHead, snackCoordinate) => {
   // console.log("before path", path.length, path.length == 0);
   path = (path.length == 0) 
   ? finder.findPath(snakeHead.x, snakeHead.y, 
     snackCoordinate.x, snackCoordinate.y, pfGrid) 
-  : path;
-  console.log('after path', path);
-
-  nextPos = path.shift();
-  if (!nextPos || (nextPos[0] == snakeHead.x && nextPos[1] == snakeHead.y)) {
-    nextPos = setNewPosition(snakeHead, snackCoordinate);
-  }
-
-  return nextPos;
-}
-
-const applySnakePosition = (prevState) => {
-  const isSnakeEating = getIsSnakeEating(prevState);
-  
-  let snakeHead = {
-    x: getSnakeHead(prevState.snake).x,
-    y: getSnakeHead(prevState.snake).y
-  };
-
-  let snakeTail = getSnakeWithoutStub(prevState.snake);
-  let snackCoordinate = prevState.snack.coordinate;
-  // console.log('snakeTail', snakeTail);
-  if (isSnakeEating) {
-    snakeTail = prevState.snake.coordinates;
-    snackCoordinate = getRandomCoordinate();
-    while (isSnake(snackCoordinate.x, snackCoordinate.y, prevState.snake.coordinates)) {
-      snackCoordinate = getRandomCoordinate();
+    : path;
+    // console.log('after path', path);
+    
+    nextPos = path.shift();
+    if (!nextPos || (nextPos[0] == snakeHead.x && nextPos[1] == snakeHead.y)) {
+      nextPos = setNewPosition(snakeHead, snackCoordinate);
     }
+    
+    return nextPos;
   }
+  
+  const applySnakePosition = (prevState) => {
+    const isSnakeEating = getIsSnakeEating(prevState);
+    
+    let snakeHead = {
+      x: getSnakeHead(prevState.snake).x,
+      y: getSnakeHead(prevState.snake).y
+    };
+    
+    let snakeTail = getSnakeWithoutStub(prevState.snake);
+    let snackCoordinate = prevState.snack.coordinate;
+    // console.log('snakeTail', snakeTail);
     if (isSnakeEating) {
-      console.info('-------------------- eating ------------------------');
+      snakeTail = prevState.snake.coordinates;
+      snackCoordinate = getRandomCoordinate();
+      while (isSnake(snackCoordinate.x, snackCoordinate.y, prevState.snake.coordinates)) {
+        snackCoordinate = getRandomCoordinate();
+      }
+    }
+    if (isSnakeEating) {
+      // console.info('-------------------- eating ------------------------');
       path = [];  
     }
     resetGridWalkableFields(prevState);
-    console.log("snakeHead", snakeHead, 'snackCoordinate', snackCoordinate);
+    // console.log("snakeHead", snakeHead, 'snackCoordinate', snackCoordinate);
     
-    console.log("nextPos before if", nextPos);
+    // console.log("nextPos before if", nextPos);
     nextPos = setNewPosition(snakeHead, snackCoordinate);
-    // if (nextPos[0] == snakeHead.x && nextPos[1] == snakeHead.y) {
-    //   nextPos = path.shift();
-    //   if (!nextPos) {
-    //     path = finder.findPath(snakeHead.x, snakeHead.y, 
-    //       snackCoordinate.x, snackCoordinate.y, pfGrid);
-    //       nextPos = path.shift();
-    //   }
-    // }
-    console.log("nextPos", nextPos);
-    console.log('after pose path', path);
+    // console.log("nextPos", nextPos);
+    // console.log('after pose path', path);
     snakeHead = {
       x:nextPos[0],
       y: nextPos[1]
     }
-    console.log('new snake head', snakeHead);
-
-
-  return {
-    snake: {
-      coordinates: [snakeHead, ...snakeTail],
-    },
-    snack: {
-      coordinate: snackCoordinate,
-    },
-    score: isSnakeEating ? prevState.score + 1 : prevState.score
-  };
-};
-
-const applyGameOver = (prevState) => ({
-  playground: {
-    isGameOver: true
-  },
-});
-
-const doChangeDirection = (direction) => () => ({
-  playground: {
-    direction,
-  },
-});
-
-const resetGame = () => ({
-  playground: {
-    direction: DIRECTIONS.RIGHT,
-    isGameOver: false,
-  },
-  snake: {
-    coordinates: [getRandomCoordinate()],
-  },
-  snack: {
-    coordinate: getRandomCoordinate(),
-  },
-  score: 0,
-});
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      playground: {
-        direction: DIRECTIONS.RIGHT,
-        isGameOver: false,
-      },
+    // console.log('new snake head', snakeHead);
+    
+    
+    return {
       snake: {
-        coordinates: [getRandomCoordinate()],
+        coordinates: [snakeHead, ...snakeTail],
       },
       snack: {
-        coordinate: getRandomCoordinate(),
+        coordinate: snackCoordinate,
       },
-      score: 0,
+      score: isSnakeEating ? prevState.score + 1 : prevState.score
     };
-  }
-
-  componentDidMount() {
-    this.interval = setInterval(this.onTick, TICK_RATE);
-
-    window.addEventListener('keyup', this.onChangeDirection, false);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-
-    window.removeEventListener('keyup', this.onChangeDirection, false);
-  }
-
-  onChangeDirection = (event) => {
-    if (KEY_CODES_MAPPER[event.keyCode]) {
-      this.setState(doChangeDirection(KEY_CODES_MAPPER[event.keyCode]));
-    } else if (KEY_CODES_MAPPER_OTHER_COMMAND[event.keyCode]) {
-      this.setState(resetGame);
+  };
+  
+  const applyGameOver = (prevState) => ({
+    playground: {
+      isGameOver: true
+    },
+  });
+  
+  const doChangeDirection = (direction) => () => ({
+    playground: {
+      direction,
+    },
+  });
+  
+  const resetGame = () => ({
+    playground: {
+      direction: DIRECTIONS.RIGHT,
+      isGameOver: false,
+    },
+    snake: {
+      coordinates: [getRandomCoordinate()],
+    },
+    snack: {
+      coordinate: getRandomCoordinate(),
+    },
+    score: 0,
+  });
+  
+  class App extends Component {
+    constructor(props) {
+      super(props);
+      
+      this.state = {
+        playground: {
+          direction: DIRECTIONS.RIGHT,
+          isGameOver: false,
+        },
+        snake: {
+          coordinates: [getRandomCoordinate()],
+        },
+        snack: {
+          coordinate: getRandomCoordinate(),
+        },
+        score: 0,
+      };
     }
-  }
-
-  onTick = () => {
-    getIsSnakeOutside(this.state.snake) || getIsSnakeClumy(this.state.snake)
+    
+    componentDidMount() {
+      this.interval = setInterval(this.onTick, TICK_RATE);
+      
+      window.addEventListener('keyup', this.onChangeDirection, false);
+    }
+    
+    componentWillUnmount() {
+      clearInterval(this.interval);
+      
+      window.removeEventListener('keyup', this.onChangeDirection, false);
+    }
+    
+    onChangeDirection = (event) => {
+      if (KEY_CODES_MAPPER[event.keyCode]) {
+        this.setState(doChangeDirection(KEY_CODES_MAPPER[event.keyCode]));
+      } else if (KEY_CODES_MAPPER_OTHER_COMMAND[event.keyCode]) {
+        this.setState(resetGame);
+      }
+    }
+    
+    onTick = () => {
+      getIsSnakeOutside(this.state.snake) || getIsSnakeClumy(this.state.snake)
       ? this.setState(applyGameOver)
       : this.setState(applySnakePosition);
-  }
-
-  render() {
-    const {
-      snake,
-      snack,
-      playground,
-      score,
-    } = this.state;
-
-    return (
-      <div className="app">
+    }
+    
+    render() {
+      const {
+        snake,
+        snack,
+        playground,
+        score,
+      } = this.state;
+      
+      return (
+        <div className="app">
         <h1>Snake! {score}</h1>
         {playground.isGameOver ? <GameOver/> : ''}
         <GridMap
-          snake={snake}
-          snack={snack}
-          isGameOver={playground.isGameOver}
+        snake={snake}
+        snack={snack}
+        isGameOver={playground.isGameOver}
         />
-      </div>
-    );
+        </div>
+      );
+    }
   }
-}
-
-const GameOver = () => 
+  
+  const GameOver = () => 
   <div className="game-over-title">
-    You loose shitty player, type "R" to reload hihi !
+  You loose shitty player, type "R" to reload hihi !
   </div>
-
-const GridMap = ({ isGameOver, snake, snack }) =>
+  
+  const GridMap = ({ isGameOver, snake, snack }) =>
   <div>
-    {GRID.map(y =>
-      <Row
-        y={y}
-        key={y}
-        snake={snake}
-        snack={snack}
-        isGameOver={isGameOver}
-      />
-    )}
+  {GRID.map(y =>
+    <Row
+    y={y}
+    key={y}
+    snake={snake}
+    snack={snack}
+    isGameOver={isGameOver}
+    />
+  )}
   </div>
-
-const Row = ({ isGameOver, snake, snack, y }) =>
+  
+  const Row = ({ isGameOver, snake, snack, y }) =>
   <div className="grid-row">
-    {GRID.map(x =>
-      <Cell
-        x={x}
-        y={y}
-        key={x}
-        snake={snake}
-        snack={snack}
-        isGameOver={isGameOver}
-      />
-    )}
+  {GRID.map(x =>
+    <Cell
+    x={x}
+    y={y}
+    key={x}
+    snake={snake}
+    snack={snack}
+    isGameOver={isGameOver}
+    />
+  )}
   </div>
-
-const Cell = ({ isGameOver, snake, snack, x, y }) =>
+  
+  const Cell = ({ isGameOver, snake, snack, x, y }) =>
   <div className={getCellCs(isGameOver, snake, snack, x, y)} />
-
-export default App;
+  
+  export default App;
